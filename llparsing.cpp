08c6a9ea157d3641a7bd53f20261025d54145bde
Arrij-Fawad <i220755@nu.edu.pk>
2025-03-22 11:35:36 +0500

    added LR

--- llparsing.cpp
+++ llparsing.cpp
@@ -7,47 +7,87 @@
 #include "LF.h"
 #include "LR.h"
 using namespace std;
+#define VERBOSE 0
 
-int main() {
-    fstream file("cfg.txt");
-    string line;
-    string nonterminal, arrow, token;
-    unordered_map<string, vector<vector<string>>> cfg;
-    
-    while (getline(file, line)) {
-        stringstream ss(line);
-        ss >> nonterminal >> arrow >> ws; // reads the arrow and whitespaces afterward to remove them from buffer        
-        vector<string> rule;  
-        
-        while (ss >> token) {
-            cout << "Token: " << token << endl;
-            if (token == "|") {
-                cfg[nonterminal].push_back(rule);                
-                rule.clear();
-            } else {
-                rule.push_back(token);
-            }
-        }
-        if (!rule.empty()) {
-            cfg[nonterminal].push_back(rule);
-        }
-    }
+void PrintCFG(unordered_map<string, vector<vector<string>>> cfg);
+unordered_map<string, vector<vector<string>>> ReadFile(string filename);
+
+int main()
+{
+
+    string filename = "cfg.txt";
+    unordered_map<string, vector<vector<string>>> cfg = ReadFile(filename);
+    cout << "----------------------------------------" << endl;
+    cout << "Original ";
+    PrintCFG(cfg);
+
     unordered_map<string, vector<vector<string>>> left_factored_cfg = left_factor(cfg);
+    cout << "----------------------------------------" << endl;
+    cout << "Left Factored ";
+    PrintCFG(left_factored_cfg);
+
+    unordered_map<string, vector<vector<string>>> noRecursion_cfg = Remove_LR(left_factored_cfg);
     
-    cout<<endl<<"Left Factored CFG: "<<endl<<endl;
-    for (auto it = left_factored_cfg.begin(); it != left_factored_cfg.end(); ++it) {
-        cout << "Nonterminal: " << it->first << " ->";
-        
-        for (int i = 0; i < it->second.size(); i++) {
+    cout << "----------------------------------------" << endl;
+    cout << "No Left Recursion ";
+    PrintCFG(noRecursion_cfg);
+
+    return 0;
+}
+
+void PrintCFG(unordered_map<string, vector<vector<string>>> cfg)
+{
+    cout << "CFG: " << endl;
+
+    for (auto it = cfg.begin(); it != cfg.end(); ++it)
+    {
+        cout << it->first << " ->";
+
+        for (int i = 0; i < it->second.size(); i++)
+        {
             cout << " ";
-            
-            for (int j = 0; j < it->second[i].size(); j++) {
+
+            for (int j = 0; j < it->second[i].size(); j++)
+            {
                 cout << it->second[i][j] << " ";
             }
             cout << "|";
         }
         cout << endl;
     }
-    Remove_LR(cfg);
-    return 0;
+    cout << endl;
+}
+unordered_map<string, vector<vector<string>>> ReadFile(string filename)
+{
+    fstream file(filename);
+    string line;
+    string nonterminal, arrow, token;
+    unordered_map<string, vector<vector<string>>> cfg;
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        ss >> nonterminal >> arrow >> ws; // reads the arrow and whitespaces afterward to remove them from buffer
+        vector<string> rule;
+
+        while (ss >> token)
+        {   if(VERBOSE)
+            cout << "Token: " << token << endl;
+            if (token == "|")
+            {
+                cfg[nonterminal].push_back(rule);
+                rule.clear();
+            }
+            else
+            {
+                rule.push_back(token);
+            }
+        }
+        if (!rule.empty())
+        {
+            cfg[nonterminal].push_back(rule);
+        }
+    }
+    file.close();
+    return cfg;
 }
